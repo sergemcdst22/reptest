b6558f1cc38fb1716537aa0146112fd1e81b5394
sergemcdst22 <sergemcdst22@gmail.com>
2022-10-08 18:08:12 +0300

    draggable

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,12 @@
-import axios from "axios";
-import React from "react";
-import ReactDOM from 'react-dom/client';
-const baseURL = "https://api.rep-test.ru";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
 
-export default function App() {
-  const [post, setPost] = React.useState(null);
+import App from "./App";
 
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  if (!post) return null;
-
-  return (
-    <div>
-      <h1>{post.Hello}</h1>
-    </div>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
