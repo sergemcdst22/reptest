e5cd94dd90189148e3261240227bed338fe7906f
sergemcdst22 <sergemcdst22@gmail.com>
2022-11-02 21:11:23 +0300

    version

--- backend/app/get_price.py
+++ backend/app/get_price.py
@@ -34,6 +34,7 @@ def get_price(articul=74249377):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
+    options.add_argument('--window-size=1920,1080')
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     browser = webdriver.Chrome(options=options) 
